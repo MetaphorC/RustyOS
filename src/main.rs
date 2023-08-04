a6797eba8bdb7cbdbfedafd14a114ff6f76adaf5
MetaphorC <ypstomar57@gmail.com>
2023-08-04 22:48:32 +0530

    Added VGA text mode functionality

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,11 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Booba Sword go Woosh!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    vga_buffer::print_something();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+    loop{}
 }
 
 mod vga_buffer;
